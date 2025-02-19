e605cf87821dab80e7d9f93a62bd18e71ad1f1ef
HaVinh <tranhavinh2003@gmail.com>
2025-02-20 03:11:41 +0700

    Updates

---
+++ precache-manifest.87019cda80d66f118727003423c358a6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b472b2df9bef665d3f8abc6af09efe9f",
+    "url": "/LinkBio/index.html"
+  },
+  {
+    "revision": "9f53800498fee2a389b6",
+    "url": "/LinkBio/static/css/main.2d960ef5.chunk.css"
+  },
+  {
+    "revision": "0135af6084ba528ddcd6",
+    "url": "/LinkBio/static/js/2.904ecfe8.chunk.js"
+  },
+  {
+    "revision": "12adf5ee6e135646e379",
+    "url": "/LinkBio/static/js/3.8ba39429.chunk.js"
+  },
+  {
+    "revision": "9f53800498fee2a389b6",
+    "url": "/LinkBio/static/js/main.044c014d.chunk.js"
+  },
+  {
+    "revision": "da1af7e3a72c85e67632",
+    "url": "/LinkBio/static/js/runtime~main.2dd4c34f.js"
+  },
+  {
+    "revision": "aac0b2b7af3a65de260083a100e0b65a",
+    "url": "/LinkBio/static/media/Qr-huyenbeo-fix.aac0b2b7.jpeg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/LinkBio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "a58a118c3ac79b64e075d21762634ff0",
+    "url": "/LinkBio/static/media/sage-avatar.a58a118c.jpeg"
+  },
+  {
+    "revision": "ec10fa7209f23e6c1a5823055c17e996",
+    "url": "/LinkBio/static/media/sage-background.ec10fa72.jpeg"
+  }
+]);

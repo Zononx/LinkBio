61e6f32a7666f6ed68934458b7df73aabf677b81
HaVinh <tranhavinh2003@gmail.com>
2025-03-05 10:38:11 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-import avatar from "./assets/sage-avatar.jpeg";
-import qrCode from "./assets/Qr-huyenbeo-fix.jpeg";
+import avatar from "./assets/meo1.jpeg";
+// import qrCode from "./assets/.jpeg";
 
-import { FaFacebook, FaInstagram } from 'react-icons/fa';
+import { FaDiscord, FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa';
 import { SiTiktok } from "react-icons/si";
 
 const ProfilePage = () => {
@@ -13,7 +13,7 @@ const ProfilePage = () => {
     <div className="bg-gray-900 text-white min-h-screen flex flex-col items-center p-6">
       <div className="text-center">
         <img src={avatar} alt="Avatar" className="avatar w-24 h-24 rounded-full mx-auto" />
-        <h2 className="text-2xl font-semibold mt-2">Huyen</h2>
+        <h2 className="text-2xl font-semibold mt-2">Zono</h2>
         {/* <p className="text-gray-400">Mọi người ủng hộ tớ nè</p> */}
         {/* <button className="mt-2 text-2xl">📷</button> */}
       </div>
@@ -21,23 +21,23 @@ const ProfilePage = () => {
       <div className="button-container w-full max-w-sm mt-4 space-y-3">
         <button 
           className="w-full bg-gray-700 py-2 rounded-lg flex items-center justify-center gap-2" 
-          onClick={() => window.location.href = "https://www.facebook.com/huyenbeo23"}
+          onClick={() => window.location.href = "https://discord.gg/hAPah8dcHe"}
         >
-          <FaFacebook className="fb text-blue-500 text-xl" />
-          Facebook
+          <FaDiscord className="fb text-blue-500 text-xl" />
+          Discord Server
         </button>
 
         <button 
           className="w-full bg-gray-700 py-2 rounded-lg flex items-center justify-center gap-2" 
-          onClick={() => window.location.href = "https://www.instagram.com/xaugaitramtinh_/"}
+          onClick={() => window.location.href = "https://www.youtube.com/@ZonoX-m1w"}
         >
-          <FaInstagram className="ig text-pink-500 text-xl" />
-          Instagram
+          <FaYoutube className="ig text-pink-500 text-xl" />
+          Youtube
         </button>
 
         <button 
           className="w-full bg-gray-700 py-2 rounded-lg flex items-center justify-center gap-2" 
-          onClick={() => window.location.href = "https://www.tiktok.com/@vocungluon"}
+          onClick={() => window.location.href = "https://www.tiktok.com/@zononx9"}
         >
           <SiTiktok className="tktk text-white text-xl" />
           TikTok
@@ -46,7 +46,7 @@ const ProfilePage = () => {
 
 
       <div className="div-img flex gap-4 mt-6">
-        <img src={qrCode} alt="QR Code" className="image-box w-40 rounded-lg" />
+        {/* <img src={qrCode} alt="QR Code" className="image-box w-40 rounded-lg" /> */}
         {/* <img src="sad-message.png" alt="Sad Message" className="w-40 rounded-lg" /> */}
       </div>
     </div>
